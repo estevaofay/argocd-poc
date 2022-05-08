e99f56360110580f3fc62849b77ce2fa54b08076
Estêvão Francisco Fáy <estevao@getmentum.com>
2022-05-08 16:00:37 -0300

    Docker iamge

--- argocd.sh
+++ argocd.sh
@@ -30,3 +30,32 @@ https://localhost:8080/argocd
 # Get password
 
 ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+
+############
+## EKSCTL ##
+############
+
+eksctl create cluster --region us-east-2 --version 1.22 --profile playground
+
+# Clone argocd repo
+git clone https://github.com/estevaofay/argocd-poc.git
+
+# Change directory
+cd argocd-poc/argocd-core-components/argocd-install
+
+# Install argocd
+kubectl apply --kustomize .
+
+# Change directory
+cd ../argocd-bootstrap
+
+# Install bootstrap
+kubectl apply --kustomize .
+
+# Change directory
+cd ..
+
+# Port-forward
+kubectl port-forward service/argocd-server -n argocd 8080:443
+
+ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
