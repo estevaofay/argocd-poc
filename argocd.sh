716fa1d095349c66b99032cff20a55f3ed55df26
Estêvão Francisco Fáy <estevao@getmentum.com>
2022-05-08 15:14:18 -0300

    Add httpbin chart

--- argocd.sh
+++ argocd.sh
@@ -7,11 +7,26 @@ minikube start
 git clone https://github.com/estevaofay/argocd-poc.git
 
 # Change directory
-cd argocd-poc/argocd-core-components
+cd argocd-poc/argocd-core-components/argocd-install
 
 # Install argocd
-kubectl apply --kustomize argocd-install
+kubectl apply --kustomize .
+
+# Change directory
+cd ../argocd-bootstrap
 
 # Install bootstrap
-kubectl apply --kustomize argocd-bootstrap
+kubectl apply --kustomize .
 
+# Change directory
+cd ..
+
+# Port-forward
+kubectl port-forward service/argocd-server -n argocd 8080:443
+
+# Login
+https://localhost:8080/argocd
+
+# Get password
+
+ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
